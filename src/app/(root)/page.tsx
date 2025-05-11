b57097ffdb8a7d68bd51e03b1e4663e93ce4284e
pekoy <mhmmdfaiz160@gmail.com>
2025-05-11 16:32:04 +0700

    feat: feature component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import { Hero } from '@/components/Hero';
+import { Feature } from '@/components/Feature';
 
 export default function LandingPage() {
   return (
     <main className='container mx-auto px-30'>
       <Hero />
+      <Feature />
     </main>
   );
 }
