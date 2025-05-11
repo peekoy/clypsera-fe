56727ae1f04a6be474ecbcd83520844bfee1cdf3
pekoy <mhmmdfaiz160@gmail.com>
2025-05-11 20:33:01 +0700

    feat: FAQ components

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import { Hero } from '@/components/Hero';
 import { Feature } from '@/components/Feature';
+import { FAQ } from '@/components/FAQ';
 
 export default function LandingPage() {
   return (
     <main className='container mx-auto px-30'>
       <Hero />
       <Feature />
+      <FAQ />
     </main>
   );
 }
