d5e37c8f2f45b7d3866e56586aabc7b9fe760d61
pekoy <mhmmdfaiz160@gmail.com>
2025-05-12 07:30:25 +0700

    chore: fix code structure

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,13 +2,22 @@ import Image from 'next/image';
 import { Hero } from '@/components/Hero';
 import { Feature } from '@/components/Feature';
 import { FAQ } from '@/components/FAQ';
+import { News } from '@/components/News';
 
 export default function LandingPage() {
   return (
-    <main className='container mx-auto px-30'>
-      <Hero />
-      <Feature />
-      <FAQ />
+    <main className='relative'>
+      <div className='absolute top-[82%] w-full flex justify-between -z-10'>
+        <Image src='/buletan-kiri.svg' alt='' width={500} height={500} />
+        <Image src='/buletan-kanan.svg' alt='' width={500} height={500} />
+      </div>
+
+      <div className='container mx-auto px-30'>
+        <Hero />
+        <Feature />
+        <FAQ />
+        <News />
+      </div>
     </main>
   );
 }
