97284fa4fb425752e38e16e930a23d615376218c
pekoy <mhmmdfaiz160@gmail.com>
2025-05-25 15:52:08 +0700

    feat: layout for privacy and term page

--- src/app/(root)/(privacy-terms)/layout.tsx
+++ src/app/(root)/(privacy-terms)/layout.tsx
@@ -1,7 +1,36 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import ScrollspySidebar from '@/components/ScrollspySidebar';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  const showScrollspy =
+    pathname?.startsWith('/privacy') || pathname?.startsWith('/terms');
+
   return (
-    <div>
-      <div>{children}</div>
-    </div>
+    <>
+      {showScrollspy ? (
+        <div className='max-w-6xl mx-auto flex flex-col px-4 py-8 space-y-20'>
+          {pathname === '/privacy' ? (
+            <p className='text-8xl text-center text-shadow-lg font-semibold primary-color'>
+              Privacy <span className='secondary-color'>Policy</span>
+            </p>
+          ) : (
+            <p className='text-8xl text-center text-shadow-lg font-semibold primary-color'>
+              Term of <span className='secondary-color'>Use</span>
+            </p>
+          )}
+          <div className='flex flex-row'>
+            <ScrollspySidebar pathname={pathname} />
+            <div className='bg-black h-auto border-1 mx-10'></div>
+            <main>{children}</main>
+          </div>
+        </div>
+      ) : (
+        <main>{children}</main>
+      )}
+    </>
   );
 }
