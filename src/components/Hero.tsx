4313c1836171b112f52a26f55dd281f69db0f56e
pekoy <mhmmdfaiz160@gmail.com>
2025-05-28 22:49:10 +0700

    fix: change hero design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,27 +4,28 @@ import Image from 'next/image';
 export async function Hero() {
   return (
     <>
-      <div className='absolute bottom-[3%] right-0 -z-10 h-full w-170'>
+      <div className='absolute left-0 bottom-[4%] -z-10 h-full w-full'>
         <Image
-          src='/bg-landingpage.png'
+          src='/homepage-hero.svg'
           alt='Background network'
-          width={1200}
-          height={800}
+          width={0}
+          height={0}
+          className='w-full'
         />
       </div>
-      <div className='flex flex-row gap-30 pt-20 pb-40'>
-        <div className='flex flex-col gap-8 justify-center w-full'>
-          <p className='text-[64px] primary-color font-bold'>
-            Smarter Healthcare <br /> Through Cleft Data
+      <div className='flex flex-row gap-30 pt-20 pb-100'>
+        <div className='flex flex-col gap-8 justify-center w-200'>
+          <p className='text-[64px] text-white font-semibold text-shadow-lg'>
+            Smarter Healthcare Through Cleft Data
           </p>
-          <p className='text-2xl font-semibold'>
+          <p className='text-2xl text-white font-semibold'>
             Clypsera is Indonesia’s trusted source for cleft lip and palate case
-            data. Our platform provides accurate, accessible, and up-to-date
+            br data. Our platform provides accurate, accessible, and up-to-date
             information to support research, healthcare, and better outcomes for
             patients.
           </p>
         </div>
-        <Image src='/cleftlip.png' alt='' width={1200} height={0}></Image>
+        {/* <Image src='/cleftlip.png' alt='' width={1200} height={0}></Image> */}
       </div>
     </>
   );
