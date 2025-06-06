560f8cedc317366a39d4eec8c2afb2f3c169cc74
pekoy <mhmmdfaiz160@gmail.com>
2025-06-06 16:43:45 +0700

    feat: upload data page

---
+++ src/app/(dashboard)/upload-data/page.tsx
@@ -0,0 +1,11 @@
+import CleftLipPatientForm from '@/components/dashboard/upload-data';
+
+export default function uploadDataPage() {
+  return (
+    <div className='p-4'>
+      <div className='max-w-4xl mx-auto'>
+        <CleftLipPatientForm />
+      </div>
+    </div>
+  );
+}
