8f418469d7041d5fbc491f7bcd2d9af3aee01d4f
pekoy <mhmmdfaiz160@gmail.com>
2025-06-07 20:11:48 +0700

    feat: user page

---
+++ src/app/(dashboard)/user/[id]/edit/page.tsx
@@ -0,0 +1,9 @@
+import EditUserForm from '@/components/dashboard/edit-user';
+
+export default function EditUserPage() {
+  return (
+    <div className='relative p-4 z-10'>
+      <EditUserForm />
+    </div>
+  );
+}
