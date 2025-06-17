08877f11637f5911d545f6d54599ed46a2a1d27c
pekoy <mhmmdfaiz160@gmail.com>
2025-06-17 20:03:43 +0700

    feat: all users interface

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,11 +1,19 @@
 import type React from 'react';
-export type UserRole = 'admin' | 'operator' | 'user';
+export type UserRole = 'admin' | 'operator' | 'user' | '';
 
-export interface User {
-  id: string;
+export interface UserAuth {
+  id: number;
   name: string;
   role: UserRole;
   avatar?: string;
+}
+
+export interface AllUsers {
+  id: number;
+  name: string;
+  email: string;
+  role: UserRole;
+  userCreationDate: string;
 }
 
 export interface MenuItem {
