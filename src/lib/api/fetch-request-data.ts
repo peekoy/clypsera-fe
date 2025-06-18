90fa2035685bccfedc61366767b1fac617b274d3
pekoy <mhmmdfaiz160@gmail.com>
2025-06-18 10:32:37 +0700

    feat: fetch api request data

---
+++ src/lib/api/fetch-request-data.ts
@@ -0,0 +1,50 @@
+import { CheckRequestData } from '@/types/check-request-data';
+
+export async function getAllRequestData(
+  token: string
+): Promise<CheckRequestData[] | null> {
+  try {
+    const res = await fetch(
+      'https://3dd8-103-194-173-98.ngrok-free.app/api/permohonan',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`,
+          'ngrok-skip-browser-warning': 'true',
+        },
+      }
+    );
+
+    console.log('tes', token);
+
+    const contentType = res.headers.get('content-type');
+
+    if (!res.ok) {
+      const text = await res.text();
+      console.error('Failed to fetch users:', res.status, text);
+      return null;
+    }
+
+    if (!contentType?.includes('application/json')) {
+      const text = await res.text();
+      console.error('Expected JSON but got:', text);
+      return null;
+    }
+
+    let data = await res.json();
+    data = data.data.map((item: any) => ({
+      id: item.id,
+      name: item.nama_pemohon,
+      email: item.email_pemohon,
+      category: item.kategori.kategori,
+      status: item.status,
+      createdAt: item.created_at,
+    }));
+    console.log('yaya', data);
+    return data;
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return null;
+  }
+}
