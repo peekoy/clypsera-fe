f31b7953d7f2166fd06de3b6befe913e172f0cef
pekoy <mhmmdfaiz160@gmail.com>
2025-06-18 10:31:14 +0700

    feat: filter mydata and request data

--- src/types/filter.ts
+++ src/types/filter.ts
@@ -8,6 +8,21 @@ export type FilterBrowse = {
   patientName: string;
 };
 
+export type FilterMyData = {
+  foundation: string;
+  operationTechnique: string;
+  gender: string;
+  age: string;
+  patientName: string;
+};
+
+export type FilterRequestData = {
+  category: string;
+  status: string;
+  email: string;
+  name: string;
+};
+
 export type FilterAdmin = {
   name: string;
   email: string;
