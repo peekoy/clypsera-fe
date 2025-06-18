c64ca80e07202a343a291b5da6545569c60bb2f2
pekoy <mhmmdfaiz160@gmail.com>
2025-06-18 10:30:25 +0700

    feat: patient data typees

---
+++ src/types/patient.ts
@@ -0,0 +1,51 @@
+export interface PatientData {
+  id: number;
+  patientName: string;
+  age: number;
+  gender: string;
+  dateOfBirth: string;
+  operationDate: string;
+  organizer: string;
+  operationalTechniques: string;
+  uploadedBy: string;
+}
+
+export interface MyDataPatient {
+  patientName: string;
+  age: number;
+  gender: string;
+  dateOfBirth: string;
+  operationDate: string;
+  organizer: string;
+  operationalTechniques: string;
+  uploadedBy: string;
+}
+
+export interface DetailedPatientData {
+  id: number;
+  name: string;
+  birthDate: string;
+  age: string;
+  address: string;
+  ethnicity: string;
+  congenitalAbnormalities: string;
+  operationDate: string;
+  surgicalTechnique: string;
+  organizer: string;
+  operationLocation: string;
+  childNumber: string;
+  gender: string;
+  cleftType: string;
+  therapyType: string;
+  diagnosis: string;
+  pregnancyHistory: string;
+  familyHistory: string;
+  relativeMarriageHistory: string;
+  previousIllnessHistory: string;
+  followUp: string;
+  uploadedBy: string;
+  creationDate: string;
+  lastUpdate: string;
+  preOpImage: string;
+  postOpImage: string;
+}
