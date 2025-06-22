4f6dd9809216dbf5f167c490c105c7e5027d468d
pekoy <mhmmdfaiz160@gmail.com>
2025-06-22 13:53:20 +0700

    feat: add user api

---
+++ src/lib/api/add-user.ts
@@ -0,0 +1,32 @@
+import { AddUserPayload } from '@/types/user';
+
+export async function addUser(payload: AddUserPayload) {
+  if (payload.password !== payload.confirmPassword) {
+    throw new Error('Password and Confirm Password do not match');
+  }
+
+  const res = await fetch(
+    'https://0b2d-118-99-106-123.ngrok-free.app/api/auth/register',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: payload.name,
+        email: payload.email,
+        password: payload.password,
+        password_confirmation: payload.confirmPassword,
+        role: payload.role,
+      }),
+    }
+  );
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || 'Failed to register');
+  }
+
+  return data;
+}
